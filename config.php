c00d376f3d226e1069c338eb578775c9fadd4099
Farlando Diengdoh <fdiengdoh@fdiengdoh.com>
2025-03-29 10:38:04 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'cache_enabled' => true,               // Enable or disable caching
+    'cache_dir'     => CACHE_DIR,  // Cache folder location
+    'cache_ttl'     => 3600,               // Time-to-live in seconds (1 hour)
+];
