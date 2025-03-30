bf90abb71f33a7f4523543c3fbe5e3c0c0411456
Farlando Diengdoh <fdiengdoh@fdiengdoh.com>
2025-03-30 20:32:19 +0530

    Update AuthController.php

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 use App\Database;
 use Delight\Auth\Auth;
 use Delight\Auth\Role;
-use App\Mailer; // Use our new Mailer class
+use App\Mailer;
 
 class AuthController {
 
