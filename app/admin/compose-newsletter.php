031b72c4ad1779c26a0cecfa00d25f6a6a7c1cdb
Farlando Diengdoh <fdiengdoh@fdiengdoh.com>
2026-03-26 12:25:37 +0530

    Update compose-newsletter.php

--- app/admin/compose-newsletter.php
+++ app/admin/compose-newsletter.php
@@ -9,131 +9,163 @@ use App\Mailer; // Assuming your Mailer class is in the App\ namespace
 AuthHelper::requireAdmin($auth, $link->getUrl('/user'));
 
 // --- PRE-FORMATTED NEWSLETTER CONTENT ---
+// This is a simple HTML email template with placeholders for dynamic content like %full_name% and %unsubscribe_link%. You can customize this template as needed.
+// Taken from https://github.com/leemunroe/responsive-html-email-template and modified for our use case.
 $defaultBodyContent = <<<HTML
-<table border="0" width="100%" cellspacing="0" cellpadding="0" bgcolor="#f6f8f1">
-<tbody>
-<tr>
-<td><table class="content" style="width: 100%; max-width: 600px;" border="0" width="100%" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff">
-<tbody>
-<tr>
-<td class="header" style="padding: 40px 30px 20px 30px;" bgcolor="#c7d8a7">
-<table border="0" width="70" cellspacing="0" cellpadding="0" align="left">
-<tbody>
-<tr>
-<td style="padding: 0 20px 20px 0;" height="70"><img class="fix" style="height: auto;" src="https://fdiengdoh.com/assets/image/fd_logo.webp" alt="" width="70" height="70" border="0"></td>
-</tr>
-</tbody>
-</table>
-<table class="col425" style="width: 100%; max-width: 425px;" border="0" cellspacing="0" cellpadding="0" align="left">
-<tbody>
-<tr>
-<td height="70">
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td class="subhead" style="font-size: 15px; color: #ffffff; font-family: sans-serif; letter-spacing: 10px; padding: 0 0 0 3px;">GREETINGS! FROM</td>
-</tr>
-<tr>
-<td class="h1" style="color: #153643; font-family: sans-serif; font-size: 33px; line-height: 38px; font-weight: bold; padding: 5px 0 0 0;">Farlando Diengdoh</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px; border-bottom: 1px solid #f2eeed;">
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td class="h2" style="color: #153643; font-family: sans-serif; padding: 0 0 15px 0; font-size: 24px; line-height: 28px; font-weight: bold;">Wishing You a Happy New Year!</td>
-</tr>
-<tr>
-<td class="bodycopy" style="color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">The year 2020 has been a year of uncertainty. This new year, I wish you and your family the best.</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px; border-bottom: 1px solid #f2eeed;">
-<table border="0" width="115" cellspacing="0" cellpadding="0" align="left">
-<tbody>
-<tr>
-<td style="padding: 0 20px 20px 0;" height="115"><img class="fix" style="height: auto;" src="http://fdiengdoh.com/assets/image/article.webp" alt="" width="115" height="115" border="0"></td>
-</tr>
-</tbody>
-</table>
-<table class="col380" style="width: 100%; max-width: 380px;" border="0" cellspacing="0" cellpadding="0" align="left">
-<tbody>
-<tr>
-<td>
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td class="bodycopy" style="color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">From my family to your family, I wish you a year of happiness, growth and prosperity. Looking forward to a better year ahead. Don't forget to keep in touch.</td>
-</tr>
-<tr>
-<td style="padding: 20px 0 0 0;">
-<table class="buttonwrapper" border="0" cellspacing="0" cellpadding="0" bgcolor="#1e4c7c">
-<tbody>
-<tr>
-<td class="button" style="text-align: center; font-size: 18px; font-family: sans-serif; font-weight: bold; padding: 0 30px 0 30px;" align="center" height="45"><a style="color: #ffffff; text-decoration: none;" href="https://www.fdiengdoh.com" target="_blank" rel="noopener">Visit my Website</a></td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td class="innerpadding borderbottom" style="padding: 30px 30px 30px 30px; border-bottom: 1px solid #f2eeed;"><img class="fix" style="height: auto;" src="https://cdn.fdh.pw/images/mawphanlur.jpg" alt="" width="100%" border="0"></td>
-</tr>
-<tr>
-<td class="innerpadding bodycopy" style="padding: 30px 30px 30px 30px; color: #153643; font-family: sans-serif; font-size: 16px; line-height: 22px;">Here is a photo I took in the year 2019 when there is no Covid-19 lockdown yet. The location is Mawphanlur Natural Lake in West Khasi Hills.</td>
-</tr>
-<tr>
-<td class="footer" style="padding: 20px 30px 15px 30px;" bgcolor="#44525f">
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td class="footercopy" style="font-family: sans-serif; font-size: 14px; color: #ffffff;" align="center">2025 © Farlando Diengdoh, Shillong<br> 
-<span style="color:#ccc">If you wish not to receive such emails you can <a href="%unsubscribe_link%" class="unsubscribe" style="text-decoration:none"><font color="#ccc">Click Here to Unsubscribe</font></a></span>
-</td>
-</tr>
-<tr>
-<td style="padding: 20px 0 0 0;" align="center">
-<table border="0" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td style="text-align: center; padding: 0 10px 0 10px;" width="37"><a href="http://www.facebook.com/fdphy"> <img style="height: auto;" src="https://cdn.fdiengdoh.com/fb.png" alt="Facebook" width="37" height="37" border="0"> </a></td>
-<td style="text-align: center; padding: 0 10px 0 10px;" width="37"><a href="http://www.youtube.com/fdphy"> <img style="height: auto;" src="https://cdn.fdiengdoh.com/yt.png" alt="Instagram" width="37" height="37" border="0"> </a></td>
-<td style="text-align: center; padding: 0 10px 0 10px;" width="37"><a href="http://www.facebook.com/fdphy"> <img style="height: auto;" src="https://cdn.fdiengdoh.com/fb.png" alt="Facebook" width="37" height="37" border="0"> </a></td>
-<td style="text-align: center; padding: 0 10px 0 10px;" width="37"><a href="http://www.twitter.com/fdiengdoh"> <img style="height: auto;" src="https://cdn.fdiengdoh.com/x.png" alt="Twitter" width="37" height="37" border="0"> </a></td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Simple Transactional Email</title>
+    <style media="all" type="text/css">
+@media all {
+  .btn-primary table td:hover {
+    background-color: #ec0867 !important;
+  }
+
+  .btn-primary a:hover {
+    background-color: #ec0867 !important;
+    border-color: #ec0867 !important;
+  }
+}
+@media only screen and (max-width: 640px) {
+  .main p,
+.main td,
+.main span {
+    font-size: 16px !important;
+  }
+
+  .wrapper {
+    padding: 8px !important;
+  }
+
+  .content {
+    padding: 0 !important;
+  }
+
+  .container {
+    padding: 0 !important;
+    padding-top: 8px !important;
+    width: 100% !important;
+  }
+
+  .main {
+    border-left-width: 0 !important;
+    border-radius: 0 !important;
+    border-right-width: 0 !important;
+  }
+
+  .btn table {
+    max-width: 100% !important;
+    width: 100% !important;
+  }
+
+  .btn a {
+    font-size: 16px !important;
+    max-width: 100% !important;
+    width: 100% !important;
+  }
+}
+@media all {
+  .ExternalClass {
+    width: 100%;
+  }
+
+  .ExternalClass,
+.ExternalClass p,
+.ExternalClass span,
+.ExternalClass font,
+.ExternalClass td,
+.ExternalClass div {
+    line-height: 100%;
+  }
+
+  .apple-link a {
+    color: inherit !important;
+    font-family: inherit !important;
+    font-size: inherit !important;
+    font-weight: inherit !important;
+    line-height: inherit !important;
+    text-decoration: none !important;
+  }
+
+  #MessageViewBody a {
+    color: inherit;
+    text-decoration: none;
+    font-size: inherit;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+  }
+}
+</style>
+  </head>
+  <body style="font-family: Helvetica, sans-serif; -webkit-font-smoothing: antialiased; font-size: 16px; line-height: 1.3; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; background-color: #f4f5f6; margin: 0; padding: 0;">
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f4f5f6; width: 100%;" width="100%" bgcolor="#f4f5f6">
+      <tr>
+        <td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top;" valign="top">&nbsp;</td>
+        <td class="container" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; max-width: 600px; padding: 0; padding-top: 24px; width: 600px; margin: 0 auto;" width="600" valign="top">
+          <div class="content" style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 600px; padding: 0;">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">This is preheader text. Some clients will show this text as a preview.</span>
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background: #ffffff; border: 1px solid #eaebed; border-radius: 16px; width: 100%;" width="100%">
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; box-sizing: border-box; padding: 24px;" valign="top">
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">Hi there %full_name%</p>
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">Sometimes you just want to send a simple HTML email with a simple design and clear call to action. This is it.</p>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; box-sizing: border-box; width: 100%; min-width: 100%;" width="100%">
+                    <tbody>
+                      <tr>
+                        <td align="left" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; padding-bottom: 16px;" valign="top">
+                          <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
+                            <tbody>
+                              <tr>
+                                <td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; border-radius: 4px; text-align: center; background-color: #0867ec;" valign="top" align="center" bgcolor="#0867ec"> <a href="http://htmlemail.io" target="_blank" style="border: solid 2px #0867ec; border-radius: 4px; box-sizing: border-box; cursor: pointer; display: inline-block; font-size: 16px; font-weight: bold; margin: 0; padding: 12px 24px; text-decoration: none; text-transform: capitalize; background-color: #0867ec; border-color: #0867ec; color: #ffffff;">Call To Action</a> </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">This is a really simple email template. It's sole purpose is to get the recipient to click the button with no distractions.</p>
+                  <p style="font-family: Helvetica, sans-serif; font-size: 16px; font-weight: normal; margin: 0; margin-bottom: 16px;">Good luck! Hope it works.</p>
+                </td>
+              </tr>
+
+              <!-- END MAIN CONTENT AREA -->
+              </table>
+
+            <!-- START FOOTER -->
+            <div class="footer" style="clear: both; padding-top: 24px; text-align: center; width: 100%;">
+              <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                <tr>
+                  <td class="content-block" style="font-family: Helvetica, sans-serif; vertical-align: top; color: #9a9ea6; font-size: 16px; text-align: center;" valign="top" align="center">
+                    <span class="apple-link" style="color: #9a9ea6; font-size: 16px; text-align: center;">Company Inc, 7-11 Commercial Ct, Belfast BT1 2NB</span>
+                    <br> Don't like these emails? <a href="%unsubscribe_link%" style="text-decoration: underline; color: #9a9ea6; font-size: 16px; text-align: center;">Unsubscribe</a>.
+                  </td>
+                </tr>
+                <tr>
+                  <td class="content-block powered-by" style="font-family: Helvetica, sans-serif; vertical-align: top; color: #9a9ea6; font-size: 16px; text-align: center;" valign="top" align="center">
+                    Powered by <a href="http://htmlemail.io" style="color: #9a9ea6; font-size: 16px; text-align: center; text-decoration: none;">HTMLemail.io</a>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <!-- END FOOTER -->
+            
+<!-- END CENTERED WHITE CONTAINER --></div>
+        </td>
+        <td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top;" valign="top">&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>
 HTML;
 
 // Initialize variables for form and feedback
