a0ff5f39ac847df5a69e65dadc81b5b72e8288e8
Farlando Diengdoh <fdiengdoh@fdiengdoh.com>
2026-03-30 10:37:03 +0530

    Update AuthConstants.php

--- src/AuthConstants.php
+++ src/AuthConstants.php
@@ -1,10 +1,21 @@
 <?php
-// src/AuthConstants.php
+declare(strict_types=1);
 
 namespace App;
 
-class AuthConstants {
-    const ROLE_ADMIN = \Delight\Auth\Role::ADMIN;
-    const ROLE_AUTHOR = \Delight\Auth\Role::AUTHOR;
-    const ROLE_SUBSCRIBER = \Delight\Auth\Role::SUBSCRIBER;
+use Delight\Auth\Role;
+
+/**
+ * Application authentication constants mapping to Delight\Auth roles.
+ */
+final class AuthConstants
+{
+    public const ROLE_ADMIN = Role::ADMIN;
+    public const ROLE_AUTHOR = Role::AUTHOR;
+    public const ROLE_SUBSCRIBER = Role::SUBSCRIBER;
+
+    private function __construct(): void
+    {
+        // Private constructor to prevent instantiation
+    }
 }
