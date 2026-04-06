1f451d818f462800abcd7cfa8e1b7e12a2dff1ca
Farlando Diengdoh <fdiengdoh@fdiengdoh.com>
2026-04-06 11:44:56 +0530

    Update AuthHelper.php

--- src/Helpers/AuthHelper.php
+++ src/Helpers/AuthHelper.php
@@ -1,23 +1,17 @@
 <?php
-declare(strict_types=1);
-
 namespace App\Helpers;
 
 use Delight\Auth\Auth;
 use Delight\Auth\Role;
 
-class AuthHelper
-{
+class AuthHelper {
     /**
      * Redirects to the login page if the user is not logged in
      * or does not have admin privileges.
      *
      * @param Auth $auth
-     * @param string $redirect
-     * @return void
      */
-    public static function requireAdmin(Auth $auth, string $redirect = '/login'): void
-    {
+    public static function requireAdmin(Auth $auth, $redirect = '/login') {
         if (!$auth->isLoggedIn() || !$auth->hasRole(Role::ADMIN)) {
             header("Location: $redirect");
             exit;
